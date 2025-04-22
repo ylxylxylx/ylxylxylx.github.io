85c8fae6ce1b395c843750131c0f0d91b51523d8
ylx <ylx@qq.com>
2025-04-22 15:55:33 +0800

    test

--- components/comments.tsx
+++ components/comments.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: ylx
+ * @Description: 
+ * @Date: 2025-04-22 15:47:27
+ * @LastEditors: ylx
+ * @LastEditTime: 2025-04-22 15:51:51
+ * @FilePath: \ylxylxylx.github.io\components\comments.tsx
+ */
 "use client";
 
 import Giscus from "@giscus/react";
@@ -5,10 +13,10 @@ import Giscus from "@giscus/react";
 export default function Comments() {
   return (
     <Giscus
-      repo="chennlang/chennlang.github.io"
-      repoId="R_kgDON2OHoA"
-      category="Comments"
-      categoryId="DIC_kwDON2OHoM4CmxNZ"
+      repo="ylxylxylx/ylxylxylx.github.io"
+      repoId="R_kgDOOdmifQ"
+      category="General"
+      categoryId="DIC_kwDOOdmifc4CpVl5"
       mapping="pathname"
       strict="0"
       reactionsEnabled="1"
